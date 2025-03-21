a4343008cf829d229f2b290eeb72723995e46ef7
gabriel acosta sanchez <121548953+puranoide@users.noreply.github.com>
2025-03-21 13:43:41 -0500

    Update db.php

--- config/db.php
+++ config/db.php
@@ -1,8 +1,9 @@
 <?php
+
 $servername = "localhost";
-$username = "u685818680_puranoadmin";
-$password = "yS1~xyK!8+";
-$database = "u685818680_SaasDental";
+$username = "root";
+$password = "";
+$database = "SaasDental";
 
 $conexion = mysqli_connect($servername,$username,$password,$database);
 
@@ -11,6 +12,12 @@ $servername = "localhost";
 $username = "u685818680_puranoadmin";
 $password = "yS1~xyK!8+";
 $database = "u685818680_SaasDental";
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "SaasDental";
+
 
 CREATE TABLE SaaS_CD.sede (
 	id INT auto_increment NOT NULL,
