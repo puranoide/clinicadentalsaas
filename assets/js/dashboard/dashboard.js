89628264a6c6d03092cacfc484460ba9c358b523
gabriel acosta sanchez <121548953+puranoide@users.noreply.github.com>
2025-04-10 16:55:54 -0500

    el formulario esta funcional

--- assets/js/dashboard/dashboard.js
+++ assets/js/dashboard/dashboard.js
@@ -348,7 +348,136 @@ function renderPacienteNoEncontrado(dni, sedesids) {
   const pError = document.createElement("p");
   pError.classList.add("error");
   pError.textContent = "Paciente no encontrado, DNI: " + dni + ", Sedes: " + sedesids.join(", ");
+  
+  const pMessage= document.createElement("p");
+  pMessage.classList.add("cuestionMensaje");
+  pMessage.textContent ="deseas registar este paciente en esta sede?"
+
+  const formAddPaciente= document.createElement("form");
+  formAddPaciente.classList.add("formAgregarPacienteCita");
+
+  //labeles
+  const labelNombres = document.createElement("label");
+  labelNombres.textContent = "Nombres:";
+  const labelDni = document.createElement("label");
+  labelDni.textContent = "DNI:";
+  const labelSede = document.createElement("label");
+  labelSede.textContent = "Sede:";
+  const labelUser = document.createElement("label");
+  labelUser.textContent = "User:";
+  const labelRole = document.createElement("label");
+  labelRole.textContent = "Role:";
+  const labelCpaciente= document.createElement("label");
+  labelCpaciente.textContent = "Codigo del paciente:";
+  const labelProcedencia= document.createElement("label");
+  labelProcedencia.textContent = "Procedencia:";
+  const labelEdad= document.createElement("label");
+  labelEdad.textContent = "Edad:";
+
+  //necesitamos craear los inputs para la creacion del paciente
+
+  const NombresP= document.createElement("input");
+  NombresP.type = "text";
+  NombresP.placeholder = "Nombres";
+  NombresP.required = true;
+
+  const inputDni = document.createElement("input");
+  inputDni.type = "text";
+  inputDni.placeholder = "DNI";
+  inputDni.value = dni;
+  inputDni.required = true;
+  inputDni.readOnly = true;
+
+  const Sede=document.createElement("input");
+  Sede.type = "number";
+  Sede.placeholder = "Sede";
+  Sede.value = sedesids;
+  Sede.required = true;
+  Sede.readOnly = true;
+  
+
+  const userRegister = document.createElement("input");
+  userRegister.type = "text";
+  userRegister.placeholder = "user";
+  userRegister.value = iduser;
+  userRegister.required = true;
+  userRegister.readOnly = true;
+
+  const roleP= document.createElement("input");
+  roleP.type = "number";
+  roleP.placeholder = "Rol";
+  roleP.value = 3;
+  roleP.required = true;
+  roleP.readOnly = true;
+
+  const cPaciente= document.createElement("input");
+  cPaciente.type = "text";
+  cPaciente.placeholder = "Codigo del paciente";
+  cPaciente.required = true;
+
+  const procedenciaP= document.createElement("input");
+  procedenciaP.type = "text";
+  procedenciaP.placeholder = "Procedencia";
+  procedenciaP.required = true;
+
+  const edadF= document.createElement("input");
+  edadF.type = "number";
+  edadF.placeholder = "Edad";
+  edadF.required = true;
+
+  const estadoP= document.createElement("input");
+  estadoP.type = "hidden";
+  estadoP.placeholder = "Estado";
+  estadoP.value = 1;
+  estadoP.required = true;
+
+  const submitButton = document.createElement("button");
+  submitButton.type = "submit";
+  submitButton.textContent = "Guardar";
+
+  //integracion de los input y labeles
+
+  formAddPaciente.appendChild(labelNombres);
+  formAddPaciente.appendChild(NombresP);
+  formAddPaciente.appendChild(labelDni);
+  formAddPaciente.appendChild(inputDni);
+  formAddPaciente.appendChild(labelSede);
+  formAddPaciente.appendChild(Sede);
+  formAddPaciente.appendChild(labelUser);
+  formAddPaciente.appendChild(userRegister);
+  formAddPaciente.appendChild(labelRole);
+  formAddPaciente.appendChild(roleP);
+  formAddPaciente.appendChild(labelCpaciente);
+  formAddPaciente.appendChild(cPaciente);
+  formAddPaciente.appendChild(labelProcedencia);
+  formAddPaciente.appendChild(procedenciaP);
+  formAddPaciente.appendChild(labelEdad);
+  formAddPaciente.appendChild(edadF);
+  formAddPaciente.appendChild(estadoP);
+  formAddPaciente.appendChild(submitButton);
+
+  submitButton.addEventListener("click", function (event) {
+    event.preventDefault();
+    const nombres = NombresP.value;
+    const dni = inputDni.value;
+    const sedevalue = Sede.value;
+    const user = userRegister.value;
+    const role = roleP.value;
+    const codigoPaciente = cPaciente.value;
+    const procedencia = procedenciaP.value;
+    const edad = edadF.value;
+    const estado = estadoP.value;
+
+    alert(`Nombres: ${nombres}, DNI: ${dni}, Sede: ${sedevalue}, User: ${user}, Role: ${role}, Codigo del paciente: ${codigoPaciente}, Procedencia: ${procedencia}, Edad: ${edad}, Estado: ${estado}`);
+  });
+
+
+
+  //integra lo necesario para la craecion del componente
   conteinerUsuario.appendChild(pError);
+  conteinerUsuario.appendChild(pMessage);
+  conteinerUsuario.appendChild(formAddPaciente);
+
 }
 
 // Inicialización
